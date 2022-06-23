9af70c714b6adb81071208e4145bb579b33b659c
mckalliprn <rilynnmckallip@gmail.com>
2022-06-23 17:10:19 -0400

    Changed path to points, attempted animation.

--- track_plot.R
+++ track_plot.R
@@ -20,14 +20,14 @@ ggplot(test_data2, mapping = aes(X, Y, color = Track, group = Track)) + geom_pat
 dat <- test_data2
 #steps is the number of frames we want the loop to walk through. For quicker testing, I have set steps to 20
 #steps <- max(dat$Frame) 
-steps <- 10
+steps <- 25
 
 #for ease of viewing, we want a consistent scale between plots, so we set limits on x and y
 #I have set xlim and ylim manually, it would be better to set them automatically to match the largest plot
 xmin <- -0.15
 xmax <- 0.15
 ymin <- -0.05
-ymax <- 1
+ymax <- 1.2
 
 #the following code should create a pdf file and plot the desired frame-by-frame plots inside
 pdf(file = paste("track_plot_output/",channel,"_plot_steps=",steps,".pdf",sep=""))
@@ -48,19 +48,41 @@ for (i in 1:steps){
   temp <- filter(dat, Frame <= i) #makes a subset of the data including only the desired frames
   #plots this subset of the data, color coordinated by track
   p <- ggplot(temp, mapping = aes(X, Y, color = Track, group = Track)) + geom_path()+ xlim(xmin,xmax) + ylim(ymin,ymax)+ ggtitle(paste("Frame",i)) 
-  append(plot_list, p)
+  plot_list[[i]] <- p
   #at the end of the loop, we should have slides that show the movement of the cells frame by frame
 }
+plot_list
 
-ggsave(filename = paste("track_plot_output/",channel,"_plot_steps=",steps,".pdf",sep=""),
-       plot = marrangeGrob(plot_list, nrow = 1, ncol = 1),
-       width = 15, height = 9)
+pdf(file = paste("track_plot_output/",channel,"_plot_steps=",steps,".pdf",sep=""))
+invisible(sapply(plot_list, print))
+dev.off()
 
-
+#making the plot into a series of points, rather than paths
+pdf(file = paste("track_plot_output/",channel,"_plot_steps=",steps,".pdf",sep=""))
+for (i in 1:steps){
+  temp <- filter(dat, Frame == i) #makes a subset of the data including only the desired frames
+  #plots this subset of the data, color coordinated by track
+  p <- ggplot(temp, mapping = aes(X, Y, color = Track, group = Track)) + geom_point()+ xlim(xmin,xmax) + ylim(ymin,ymax)+ ggtitle(paste("Frame",i)) 
+  print(p)
+  #ggsave(paste("track_plot_output/testingPlots_",i,".png",sep=""),p)
+  #at the end of the loop, we should have slides that show the movement of the cells frame by frame
+}
+dev.off()
+ggplot(temp, mapping = aes(X, Y, color = Track, group = Track)) + geom_point()+ xlim(xmin,xmax) + ylim(ymin,ymax) 
 
 #in this next bit of code, we will create a gif which shows the movement of the different tracks over time
 #following code doesn't yet work
-p <- ggplot(dat, aes(X, Y, color = Track, group = Track)) + geom_path() + xlim(xmin,xmax) + ylim(ymin,ymax)+ ggtitle(paste("Frame",i)) 
+p <- ggplot(dat, aes(X, Y, color = Track, group = Track)) + 
+  geom_point() + 
+  xlim(xmin,xmax) + 
+  ylim(ymin,ymax)
 print(p)
-p + transition_time(Frame) + labs(title = 'Frame: {frame_time}', x = 'X position', y = 'Y position')  + ease_aes('linear')
+p + labs(title = 'Frame: {Frame}', 
+         x = 'X position', 
+         y = 'Y position') + 
+  transition_manual(Frame) + ease_aes('linear')
+
+
+
+
 
